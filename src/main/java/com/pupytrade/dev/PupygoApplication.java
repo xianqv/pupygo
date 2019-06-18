a9b666be2f1314452ce5a1a60f45a398ee749b70
haigang <haigang.deng@binqsoft.com>
2019-06-18 13:17:36 +0800

    create this project

---
+++ src/main/java/com/pupytrade/dev/PupygoApplication.java
@@ -0,0 +1,13 @@
+package com.pupytrade.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupygoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PupygoApplication.class, args);
+    }
+
+}
